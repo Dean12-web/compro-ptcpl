226c8de5f6956dc090955569f954e470b7f108ca
Dean12-web <mahyudinakbar10@gmail.com>
2026-03-16 16:47:58 +0700

    fix contact seeder

--- database/seeders/ContactContentBlockSeeder.php
+++ database/seeders/ContactContentBlockSeeder.php
@@ -14,42 +14,6 @@ class ContactContentBlockSeeder extends Seeder
 
         foreach ($locales as $locale) {
 
-            /*
-            HERO SECTION
-            */
-            $hero = ContentBlock::create([
-                'key' => 'contact_hero_section',
-                'block_type' => 'single',
-                'title' => 'CONTACT',
-                'sort_order' => 1,
-                'locale' => $locale,
-                'is_active' => true
-            ]);
-
-            $this->createItems($hero->id,[
-                ['title','Title','text'],
-                ['description','Description','textarea'],
-                ['hero_image','Hero Image','image']
-            ]);
-
-
-            /*
-            CONTACT FORM SECTION
-            */
-            $form = ContentBlock::create([
-                'key' => 'contact_form_section',
-                'block_type' => 'single',
-                'title' => 'CONTACT',
-                'sort_order' => 2,
-                'locale' => $locale,
-                'is_active' => true
-            ]);
-
-            $this->createItems($form->id,[
-                ['form_title','Form Title','text'],
-                ['submit_button_text','Submit Button Text','text']
-            ]);
-
 
             /*
             CONTACT INFORMATION
@@ -64,17 +28,6 @@ class ContactContentBlockSeeder extends Seeder
             ]);
 
             $this->createItems($info->id,[
-                ['office_title','Office Title','text'],
-                ['office_address','Office Address','textarea'],
-
-                ['email_title','Email Title','text'],
-                ['email_primary','Primary Email','text'],
-                ['email_secondary','Secondary Email','text'],
-
-                ['phone_title','Phone Title','text'],
-                ['phone_number','Phone Number','text'],
-                ['phone_hours','Phone Hours','text'],
-
                 ['map_image','Map Image','image']
             ]);
 
